e711ecb8dc7d5eba22510b4b9e49fcc99f80f804
kvzhukovskiy <63715298+kvzhukovskiy@users.noreply.github.com>
2021-04-23 01:50:02 +0300

    lab 9-2 fixed

--- 09-3.c
+++ 09-3.c
@@ -4,21 +4,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/sem.h>
-#include <time.h>
 
-//Function A is increment func for sem
-//Function D is decrement func for sem
 //Start value is 0
+//Parent: for{write() => A(+2) => Z => read}
+//Child: for{D(-1) => read() => write() => D(-1)}
 
-int A(int id, struct sembuf *buffer){
-	buffer->sem_op = 1;
+int A(int id, struct sembuf *buffer, int val){
+	buffer->sem_op = val;
 	buffer->sem_flg = 0;
 	buffer->sem_num = 0;
 	return semop(id, buffer, 1);
 }
 
-int D(int id, struct sembuf *buffer){
-	buffer->sem_op = -1;
+int D(int id, struct sembuf *buffer, int val){
+	buffer->sem_op = val;
+	buffer->sem_flg = 0;
+	buffer->sem_num = 0;
+	return semop(id, buffer, 1); 
+}
+
+int Z(int id, struct sembuf *buffer){
+	buffer->sem_op = 0;
 	buffer->sem_flg = 0;
 	buffer->sem_num = 0;
 	return semop(id, buffer, 1); 
@@ -28,8 +34,8 @@ int main(){
 	int ptc[2], semid, pid;
 	key_t key;
 	size_t N, size;
+	char buf[15];
 	char path[] = "09-3.c";
-	char buf[22]; 
 	struct sembuf buffer;
 
 	if(pipe(ptc) < 0){ 
@@ -48,63 +54,77 @@ int main(){
 	  	return -1;
 	}
 
-  	pid = fork(); 
+	printf("Input N > 1\n");
+	scanf("%d", &N);
+	if(N < 2){
+		printf("N must be 2 or higher\n");
+		return -1; 
+	}
+
+  	pid = fork();
+ 
 	if(pid < 0){ 
 		printf("Can't create fork\n");
 		return -1;
 	}else if (pid > 0){
 		//Parent code 
-		printf("Input N > 1\n");
-		scanf("%d", &N);
-		if(N < 2){
-			A(semid, &buffer);
-			printf("N must be 2 or higher\n");
-			return -1; 
-			}
-    
 		for(int i = 0; i < N; i++){
-			size = write(ptc[1], "Hello world to child!!", 22);
-			if(size != 22){
+			size = write(ptc[1], "Hello, child!", 14);
+			if(size != 14){
 				printf("Parent: Can't write full string\n");
 				return -1;
 			}
-			//Writing a message for child
-			//And give him rules
-			A(semid, &buffer);
-			D(semid, &buffer);
 
-			size = read(ptc[0], buf, 22);
+			A(semid, &buffer, 2);
+			if(semop(semid, &buffer, 1) < 0){
+				printf("Can't increase value\n");
+				return -1;
+			}
+
+			Z(semid, &buffer);
+			if(semop(semid, &buffer, 1) < 0){
+				printf("Can't Z\n");
+				return -1;
+			}
+
+			size = read(ptc[0], buf, 15);
        		if(size < 0){
-           		printf("Parent: Can\'t read string\n"); 
+           		printf("Parent: Can't read string\n"); 
            		return -1;
         	} 
 			printf("Parent got message: [%s]\n", buf);
     	}
-    	close(ptc[0]);
+		printf("Parent finished working");
 	}else{ 
 		//Child code  
-		for(;;){
-			//Waiting for parent message
-			D(semid, &buffer);
-      		size = read(ptc[0], buf, 22);
+		for(int i = 0; i < N; i++){
+			D(semid, &buffer, -1);
+			if(semop(semid, &buffer, 1) < 0){
+				printf("Can't decrease value\n");
+				return -1;
+			}
+
+      		size = read(ptc[0], buf, 14);
       		if(size < 0){
-        		printf("Child: Can\'t read string\n"); 
+        		printf("Child: Can't read string\n"); 
         		return -1;
       		}
       		printf("Child got message: [%s]\n", buf);
    
-      		size = write(ptc[1], "Hello world to parent!", 22);
-      		if(size != 22){
+      		size = write(ptc[1], "Hello, parent!", 15);
+      		if(size != 15){
         		printf("Child: Can\'t write full string\n");
         		return -1;
       		}
-			//Writing a meesage for parent
-			//And give back rules
-      		A(semid, &buffer);
+
+      		D(semid, &buffer, -1);
+			if(semop(semid, &buffer, 1) < 0){
+				printf("Can't increase value\n");
+				return -1;
+			}
     	} 
+		printf("Child finished working");
   	}
-	//Return start value for new launch
-	A(semid, &buffer);
 	close(ptc[0]);
 	close(ptc[1]);	
 	return 0; 
